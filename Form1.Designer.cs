84e6d4305653cd9435cb087a35b60573182a2d8f
Juan Albert <juan_ruas@outlook.com>
2026-08-14 16:37:38 -0300

    Commit inicial: Hello World

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace erp_mendonca_e_freitas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_HW = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_HW
+            // 
+            this.lbl_HW.AutoSize = true;
+            this.lbl_HW.Location = new System.Drawing.Point(327, 198);
+            this.lbl_HW.Name = "lbl_HW";
+            this.lbl_HW.Size = new System.Drawing.Size(70, 25);
+            this.lbl_HW.TabIndex = 0;
+            this.lbl_HW.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_HW);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_HW;
+    }
+}
+
